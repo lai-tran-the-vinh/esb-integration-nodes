ea5c29cc6f9a30de099952e0b9ae46d92401460b
lai-tran-the-vinh <thevinh.development@gmail.com>
2026-07-31 08:22:13 +0700

    feat: add dotenv for environment variable management and update .gitignore

--- web-shop/app.js
+++ web-shop/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -8,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); // Đọc form data
 app.use(express.json());
 
-app.use('/', require('./routes/index.route.js'));
+app.use('/', require('./src/routes/index.route.js'));
 
 app.listen(3000, () => {
     console.log('Web Mua Hàng MVC đang chạy tại: http://localhost:3000');
